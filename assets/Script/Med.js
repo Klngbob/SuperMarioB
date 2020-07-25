d3d9ca1b12001aa3e5b400369ce341e58b3558bb
Klngbob <529636014@qq.com>
2020-07-25 14:11:34 +0800

    7.25_HP++

---
+++ assets/Script/Med.js
@@ -0,0 +1,50 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //药水碰撞半径
+        pickRadius:0,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad:function () {
+    },
+
+    getPlayerDistance:function(){
+        var player=cc.find("Canvas/Player");
+        var playerPos=player.getPosition();
+        //console.log(this.node.x,this.node.y);
+        var dist = this.node.position.sub(playerPos).mag();
+        //console.log("dist:",dist);
+        return dist;
+    },
+
+    onPicked:function(){
+        var backnode=cc.find("Canvas/Player/HP");
+        var hp=backnode.getComponent(cc.ProgressBar);
+        //console.log(hp.progress);
+        hp.progress+=0.2;
+        //console.log(hp.progress);
+        this.node.destroy();
+    },
+    start () {
+
+    },
+
+    update:function (dt) {
+        if(this.getPlayerDistance()<this.pickRadius){
+            //调用收集行为
+            console.log("yes");
+            this.onPicked();
+            return;
+        }
+    },
+});
